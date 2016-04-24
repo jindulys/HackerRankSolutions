07f6eb5e4740dde6302bb8e26511cbebc65989a5
yansong li <857367901@qq.com>
2016-04-24 11:27:07 -0400

    Removed test in vc

--- HRSwift/ViewController.swift
+++ HRSwift/ViewController.swift
@@ -13,8 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let maximiseSum = MaximiseSum()
-        maximiseSum.localTest()
     }
 
     override func didReceiveMemoryWarning() {
