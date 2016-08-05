b1dec5f39c93816ede5d9ed9cd9ea18f7338f985
yansong li <857367901@qq.com>
2016-08-04 20:47:18 -0400

    Create LeetCodeKit, where stores all kind of common code for leetcode.

---
+++ Sources/Leetcode/LeetCodeKit.swift
@@ -0,0 +1,22 @@
+//
+//  LeetCodeKit.swift
+//  HRSwift
+//
+//  Created by yansong li on 2016-08-04.
+//  Copyright © 2016 yansong li. All rights reserved.
+//
+
+import Foundation
+
+// MARK: TreeNode
+
+public class TreeNode {
+  public var val: Int
+  public var left: TreeNode?
+  public var right: TreeNode?
+  public init(_ val: Int) {
+    self.val = val
+    self.left = nil
+    self.right = nil
+  }
+}
--- Sources/Leetcode/Tree/100_SameTree.swift
+++ Sources/Leetcode/Tree/100_SameTree.swift
@@ -8,17 +8,6 @@
 
 import Foundation
 
-public class TreeNode {
-  public var val: Int
-  public var left: TreeNode?
-  public var right: TreeNode?
-  public init(_ val: Int) {
-    self.val = val
-      self.left = nil
-      self.right = nil
-  }
-}
-
 /**
   Given two binary trees, write a function to check if they are equal or not.
   
